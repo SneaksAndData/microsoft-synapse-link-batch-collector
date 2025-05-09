99df106b8f4b625e9f26f666b6ee2a0f3dc9dd77
George Zubrienko <gzu@ecco.com>
2025-05-09 13:40:18 +0200

    Parallelize delete operations and remove HNS folder at the end

--- microsoft_synapse_batch_collector/processing/azure_cleanup.py
+++ microsoft_synapse_batch_collector/processing/azure_cleanup.py
@@ -1,9 +1,13 @@
 """
  Cleanup.
 """
+import os
+
 from adapta.logs import LoggerInterface
 from adapta.metrics import MetricsProvider
 from adapta.storage.blob.azure_storage_client import AzureStorageClient
+from adapta.storage.models import AdlsGen2Path
+from adapta.utils.concurrent_task_runner import Executable, ConcurrentTaskRunner
 
 from microsoft_synapse_batch_collector.models.uploaded_batch import UploadedBatch
 
@@ -21,6 +25,16 @@ def remove_batch(
     :param metrics: MetricsProvider
     :return:
     """
+
+    def _delete_blob(blob: AdlsGen2Path) -> bool:
+        if not dry_run:
+            logger.info("Deleting blob: {blob}", blob=blob.path)
+            client.delete_leased_blob(blob)
+            return True
+        else:
+            logger.info("Blob: {blob} qualifies for deletion. Skipping due to dry_run set to True", blob=blob.path)
+            return False
+
     logger.info("Removing archived Synapse batch {batch}", batch=batch.source_path.path)
     metrics.gauge(
         metric_name="batch.total_blobs",
@@ -29,11 +43,18 @@ def remove_batch(
             "batch": batch.source_path.path.split("/")[0],
         },
     )
-    for blob_to_delete in batch.blobs:
-        if not dry_run:
-            logger.info("Deleting blob: {blob}", blob=blob_to_delete.path)
-            client.delete_leased_blob(blob_to_delete)
-        else:
-            logger.info(
-                "Blob: {blob} qualifies for deletion. Skipping due to dry_run set to True", blob=blob_to_delete.path
-            )
+    blob_delete_tasks = [
+        Executable(
+            func=_delete_blob,
+            args=[
+                blob_to_delete,
+            ],
+            alias=blob_to_delete.path,
+        )
+        for blob_to_delete in batch.blobs
+    ]
+    _ = ConcurrentTaskRunner(blob_delete_tasks, int(os.getenv("CLEANUP_THREADS", "128")), False).eager()
+
+    # delete now-empty prefix - in case HNS is enabled, it will be retained as empty folder
+    if os.getenv("HNS_ENABLED", "0") == "1":
+        client.delete_blob(batch.source_path)
